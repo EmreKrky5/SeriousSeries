1a3e016268faef375223e96adf9cf24f4cb1f801
Cem Karayünlü <cem@Cems-MacBook-Pro.local>
2016-04-11 20:44:21 +0300

    Log in ekranı açıldı
    Log in ekranı açıldı ve arka plan rengi logoya daha uygun hale geldi.

--- app/src/main/java/com/example/win7/seriousseries/MainActivity.java
+++ app/src/main/java/com/example/win7/seriousseries/MainActivity.java
@@ -18,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoadImage.class));
             }
         });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
 
 
     }
