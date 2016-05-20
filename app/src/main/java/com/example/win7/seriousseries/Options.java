bd0f65d122512feb2e202445cfabe01fe7a549c5
EmreKrky5 <emre.karakaya@std.ieu.edu.tr>
2016-05-20 10:49:00 +0300

    Sayfa geçişleri ayarlandı, load image'e bakılacak

--- app/src/main/java/com/example/win7/seriousseries/Options.java
+++ app/src/main/java/com/example/win7/seriousseries/Options.java
@@ -1,7 +1,9 @@
 package com.example.win7.seriousseries;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Options extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Options extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        findViewById(R.id.button11).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Options.this, Notifications.class));
+            }
+        });
     }
 }
