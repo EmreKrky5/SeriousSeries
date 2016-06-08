c57e0695b95537bf220623b6c29f8516c6d159a2
Cem <Cem>
2016-06-08 13:50:40 +0300

    diziler eklendi
    ref: https://www.youtube.com/watch?v=5nZMoBjq6Qg

--- app/src/main/java/com/echo/seriouseries/FavoriteSeries.java
+++ app/src/main/java/com/echo/seriouseries/FavoriteSeries.java
@@ -18,10 +18,67 @@ import java.util.List;
 
 public class FavoriteSeries extends AppCompatActivity {
 
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] android_versions = {
+    "Game of Thrones",
+    "Arrow",
+    "The Walking Dead",
+    "Sherlock"
+    //The Flash,
+    //The Big Bang Theory,
+    //        Supernatural,
+    //        Gotham,
+    //Marvel’s Agents of S.H.I.E.L.D,
+    //Vikings,
+    //Breaking Bad,
+            //Once Upon a Time,
+            //The Vampire Diaries,
+            //American Horror Story,
+            //The 100,
+            //Suits,
+            //Under The Dome,
+            //True Detective,
+            //Da Vinci’s Demons,
+            //How I Met Your Mother,
+            //The Blacklist,
+            //        Hannibal,
+            //Person of Interest,
+            //The Originals,
+            //        Homeland,
+            //        Dexter,
+            //Orphan Black,
+            //Orange Is The New Black,
+            //Teen Wolf,
+            //        Elementary,
+            // New Girl,
+            // Doctor Who,
+            //         Fargo,
+            // Modern Family,
+            //         Shameless,
+            // House of Cards,
+            //         2 Broke Girls,
+            // Sleepy Hollow,
+            //         Constantine,
+            //         Grimm,
+            // The Mentalist,
+            //         Forever,
+            //         House,
+            // Marvel’s Daradevil,
+            //         Revolution,
+            // Bates Motel,
+            // The Following,
+            // Pretty Little Liars,
+            // Falling Skies,
+            //         Continuum
 
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_series);
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1), android_versions);
+        listView.setAdapter(adapter);
     }
 }
 
